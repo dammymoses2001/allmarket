f050eb1538044e4e4c52a659e6f74fec8605deed
Dammy <dammymoses2001@gmail.com>
2020-11-11 11:48:43 +0100

    update

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,3 +1,4 @@
 export const api = 'https://obscure-savannah-07932.herokuapp.com/';
 // https://obscure-savannah-07932.herokuapp.com/
+//https://obscure-savannah-07932.herokuapp.com/
 //http://localhost:3000/
